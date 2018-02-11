9ad4b56d359d915c1b516965787a83f67738c6db
shige0409 <basketball0316pf@gmail.com>
2018-02-11 16:41:10 +0900

    exit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'comment/new'
+
+  get 'comment/show'
+
+  get 'comment/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
